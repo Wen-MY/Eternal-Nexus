9c3fded1fd06171ec77bf895d41b0d68d4efb6d5
Bruce Wayne <tanqrystal11.ct@gmail.com>
2023-09-17 09:54:59 +0800

    Main Menu Done, Show Ammo, Skill PlayerPref Link

---
+++ Assets/ToolTip/ToolTipManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ToolTipManager : MonoBehaviour
+{
+    public static ToolTipManager _instance;
+    public TextMeshProUGUI textComponent;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+
+    public void ShowToolTip(string message, Vector3 position)
+    {
+        gameObject.SetActive(true);
+        textComponent.text = message;
+        transform.position = position; // Set the tooltip's position
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        textComponent.text = string.Empty;
+    }
+}
